57b84613f7e58cc18b65cc09e36931cd2d5f97b5
bastianl <bastian.lennart@gmail.com>
2017-01-31 14:40:45 -0800

    scaffold for the genres model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :genres
   resources :movies
   resources :users, :only => [:show, :index, :update]
 end
---
+++ spec/factories/genres.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :genre do
+    genre "MyString"
+  end
+end
